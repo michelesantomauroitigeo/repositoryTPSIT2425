3767517e8e39fad7506fceb424629c27384db276
roccolig18 <ligraniroccomaria@icloud.com>
2024-12-10 18:29:21 +0100

    Aggiunta JavaScript

---
+++ LigraniR/4I_06/www/js/script.js
@@ -0,0 +1,14 @@
+// Funzione per cambiare il testo
+function modificaScritta() { 
+    document.getElementById("prova").innerHTML = "Scopri come una buona illuminazione puo' cambiare il tuo ambiente!";
+}
+
+// Funzione per impostare l'immagine della luce calda
+function luceCalda() {
+    document.getElementById('img').src = './img/luce_calda.png';  // Cambia l'immagine della luce calda
+}
+
+// Funzione per impostare l'immagine della luce fredda
+function luceFredda() {
+    document.getElementById('img').src = './img/luce_fredda.JPG';  // Cambia l'immagine della luce fredda
+}
