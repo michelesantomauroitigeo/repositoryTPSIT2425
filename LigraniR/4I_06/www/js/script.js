5e01cf6bb9c14164b858bd62b001f19960a44711
roccolig18 <ligraniroccomaria@icloud.com>
2024-12-10 18:59:15 +0100

    Aggiunta JavaScript

--- LigraniR/4I_06/www/js/script.js
+++ LigraniR/4I_06/www/js/script.js
@@ -5,10 +5,10 @@ function modificaScritta() {
 
 // Funzione per impostare l'immagine della luce calda
 function luceCalda() {
-    document.getElementById('img').src = './img/luce_calda.png';  // Cambia l'immagine della luce calda
+    document.getElementById('img').src = '../imgs/luce_calda.png';  // Cambia l'immagine della luce calda
 }
 
 // Funzione per impostare l'immagine della luce fredda
 function luceFredda() {
-    document.getElementById('img').src = './img/luce_fredda.JPG';  // Cambia l'immagine della luce fredda
+    document.getElementById('img').src = '../imgs/luce_fredda.JPG';  // Cambia l'immagine della luce fredda
 }
