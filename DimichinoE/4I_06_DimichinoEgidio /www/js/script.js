077eca137b98d8ed7219af33ca4868b57cf1d2e9
Egidio13 <egidiodimichino@gmail.com>
2024-12-10 20:22:23 +0100

    Lavoro Dimichino JavaScript
    Semaforo

---
+++ DimichinoE/4I_06_DimichinoEgidio /www/js/script.js
@@ -0,0 +1,9 @@
+function semafororosso() {
+  document.getElementById("Semaforo").src = "imgs/rosso.png";
+}
+function semaforoGiallo() {
+  document.getElementById("Semaforo").src = "imgs/giallo.png";
+}
+function semaforoVerde() {
+  document.getElementById("Semaforo").src = "imgs/verde.png";
+}
