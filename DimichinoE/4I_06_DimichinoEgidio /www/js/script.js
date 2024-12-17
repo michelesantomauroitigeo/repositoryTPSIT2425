58368579ac29460aeb97e0fa71e01edaa93250a5
Egidio13 <egidiodimichino@gmail.com>
2024-12-17 10:36:11 +0100

    Rimossa cartella 06

--- DimichinoE/4I_06_DimichinoEgidio /www/js/script.js
+++
@@ -1,9 +0,0 @@
-function semafororosso() {
-  document.getElementById("Semaforo").src = "imgs/rosso.png";
-}
-function semaforoGiallo() {
-  document.getElementById("Semaforo").src = "imgs/giallo.png";
-}
-function semaforoVerde() {
-  document.getElementById("Semaforo").src = "imgs/verde.png";
-}
