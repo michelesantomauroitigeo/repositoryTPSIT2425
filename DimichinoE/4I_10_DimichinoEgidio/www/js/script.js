d6602bda83dd0b9365260b00ebef325f310cf2c5
Egidio13 <egidiodimichino@gmail.com>
2025-02-16 17:30:50 +0100

    Continuo lavoro 010

--- DimichinoE/4I_10_DimichinoEgidio/www/js/script.js
+++ DimichinoE/4I_10_DimichinoEgidio/www/js/script.js
@@ -5,10 +5,21 @@ function Modifica(){
 function chiusuraFinestra(){
     document.getElementById('finestra').style.display='none';
 }
+
+
 function elimina(){
-    document.getElementById('finestra').style.display='block';
+    document.getElementById('finestra1').style.display='block';
 }
 
-function chiusuraFinestra(){
-    document.getElementById('finestra').style.display='none';
+function chiusuraFinestra1(){
+    document.getElementById('finestra1').style.display='none';
+}
+
+
+function Abilita(){
+    document.getElementById('finestra2').style.display='block';
+}
+
+function chiusuraFinestra2(){
+    document.getElementById('finestra2').style.display='none';
 }
