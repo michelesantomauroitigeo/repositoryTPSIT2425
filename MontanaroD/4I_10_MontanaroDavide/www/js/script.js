50101ca22461d7c6a90a3e63905192a419e974b2
Davidemont2007 <davidemontanaro2@gmail.com>
2025-02-25 22:40:17 +0100

    10

--- MontanaroD/4I_10_MontanaroDavide/www/js/script.js
+++
@@ -1,8 +0,0 @@
-function Modifica(){
-    document.getElementById('finestra').style.display='block';
-}
-
-function chiusuraFinestra(){
-    document.getElementById('finestra').style.display='none';
-}
-
