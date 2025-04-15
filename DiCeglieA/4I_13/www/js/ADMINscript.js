99da95f1829ad6b2977afcfa42cc82775ce78bba
Kemkera26 <adiceglie26@gmail.com>
2025-04-15 02:06:59 +0200

    aggiornamento prog 13

---
+++ DiCeglieA/4I_13/www/js/ADMINscript.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById("modal");
+
+var button = document.getElementById("button");
+
+var TastoChiusura = document.getElementById("close");
+
+button.onclick = function () {
+  modal.style.display = "block";
+};
+
+TastoChiusura.onclick = function () {
+  modal.style.display = "none";
+};
+
+
+/*PRODOTTI SCRIPT*/
+
